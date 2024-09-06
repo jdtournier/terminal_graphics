b6c5fa335ed57b0653ce2c47afbcb543820389b0
J-Donald Tournier <jdtournier@gmail.com>
2024-09-06 13:36:14 +0100

    handle comments in PGM files correctly

--- pgm.h
+++ pgm.h
@@ -3,6 +3,7 @@
 
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <exception>
 #include <limits>
 
@@ -19,13 +20,20 @@ Image<ValueType> load_pgm (const std::string& pgm_filename)
   if (!in)
     throw std::runtime_error ("failed to open input PGM file \"" + pgm_filename + "\"");
 
+  // strip out all comments, and present stringstream of clean file contents:
+  std::string line;
+  std::stringstream in_clean;
+  while (std::getline (in, line)) {
+    in_clean << line.substr (0, line.find_first_of('#')) << "\n";
+  }
+
   std::string magic;
-  in >> magic;
+  in_clean >> magic;
   if (magic != "P2")
     throw std::runtime_error ("input file \"" + pgm_filename + "\" is not in expected PGM format");
 
   int xdim, ydim, maxval;
-  in >> xdim >> ydim >> maxval;
+  in_clean >> xdim >> ydim >> maxval;
 
   if (maxval >= 65536)
     throw std::runtime_error ("PGM file \"" + pgm_filename + "\" is badly formed: maxval exceeds 65536");
@@ -43,7 +51,7 @@ Image<ValueType> load_pgm (const std::string& pgm_filename)
       // as extracting these types from stream converts individual characters,
       // not full ASCII-encoded integers.
       double val;
-      in >> val;
+      in_clean >> val;
       im(x,y) = static_cast<ValueType> (val);
     }
   }
