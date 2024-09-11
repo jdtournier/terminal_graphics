84b5c37d927070a797bb9019985cf416179f4923
J-Donald Tournier <jdtournier@gmail.com>
2024-09-11 15:30:57 +0100

    plot.h: fix up various issues and allow variable font size

--- plot.h
+++ plot.h
@@ -24,18 +24,22 @@ namespace Sixel {
 
       Plot& set_colourmap (const Sixel::ColourMap& colourmap);
 
-      Plot& add_line (float x0, float y0, float x1, float y1, int colour_index = 2, int stiple = 0, float stiple_frac = 0.5);
+      Plot& add_line (float x0, float y0, float x1, float y1,
+          int colour_index = 2, int stiple = 0, float stiple_frac = 0.5);
 
       template <class VerticesType>
-        Plot& add_line (const VerticesType& y, int colour_index = 2, int stiple = 0, float stiple_frac = 0.5);
+        Plot& add_line (const VerticesType& y,
+            int colour_index = 2, int stiple = 0, float stiple_frac = 0.5);
 
       template <class VerticesType>
-        Plot& add_line (const VerticesType& x, const VerticesType& y, int colour_index = 2, int stiple = 0, float stiple_frac = 0.5);
+        Plot& add_line (const VerticesType& x, const VerticesType& y,
+            int colour_index = 2, int stiple = 0, float stiple_frac = 0.5);
 
-      Plot& add_text (const std::string& text, float x, float y, float anchor_x = 0.5, float anchor_y = 0.5, int colour_index = 1);
+      Plot& add_text (const std::string& text, float x, float y,
+          float anchor_x = 0.5, float anchor_y = 0.5, int font_size = 6, int colour_index = 1);
 
-      Plot& set_xlim (float min, float max);
-      Plot& set_ylim (float min, float max);
+      Plot& set_xlim (float min, float max, float expand_by = 0.0);
+      Plot& set_ylim (float min, float max, float expand_by = 0.0);
       Plot& set_grid (float x_interval, float y_interval);
 
     private:
@@ -48,7 +52,8 @@ namespace Sixel {
       int margin_x, margin_y;
 
       template <class ImageType>
-        static void line_x (ImageType& canvas, float x0, float y0, float x1, float y1, int colour_index, int stiple, float stiple_frac);
+        static void line_x (ImageType& canvas, float x0, float y0, float x1, float y1,
+            int colour_index, int stiple, float stiple_frac);
 
       float mapx (float x) const;
       float mapy (float y) const;
@@ -69,9 +74,11 @@ namespace Sixel {
   // **************************************************************************
 
 
+  constexpr float lim_expand_by_factor = 0.1f;
 
   template <class ImageType>
-    inline void Plot::line_x (ImageType& canvas, float x0, float y0, float x1, float y1, int colour_index, int stiple, float stiple_frac)
+    inline void Plot::line_x (ImageType& canvas, float x0, float y0, float x1, float y1,
+        int colour_index, int stiple, float stiple_frac)
     {
       if (x0 > x1) {
         std::swap (x0, x1);
@@ -161,28 +168,30 @@ namespace Sixel {
     return *this;
   }
 
-  inline Plot& Plot::set_xlim (float min, float max)
+  inline Plot& Plot::set_xlim (float min, float max, float expand_by)
   {
     if (std::isfinite (xlim[0]) || std::isfinite (xlim[1])) {
       std::cerr << "WARNING: xlim already set (maybe implicitly by previous calls - call set_xlim() before any draw calls" << std::endl;
     }
     else {
-      xlim[0] = min;
-      xlim[1] = max;
+      const float delta = expand_by * (max - min);
+      xlim[0] = min - delta;
+      xlim[1] = max + delta;
       if (!std::isfinite (xgrid))
         xgrid = (xlim[1] - xlim[0])/5.0;
     }
     return *this;
   }
 
-  inline Plot& Plot::set_ylim (float min, float max)
+  inline Plot& Plot::set_ylim (float min, float max, float expand_by)
   {
     if (std::isfinite (ylim[0]) || std::isfinite (ylim[1])) {
       std::cerr << "WARNING: ylim already set (maybe implicitly by previous calls - call set_ylim() before any draw calls" << std::endl;
     }
     else {
-      ylim[0] = min;
-      ylim[1] = max;
+      const float delta = expand_by * (max - min);
+      ylim[0] = min - delta;
+      ylim[1] = max + delta;
       if (!std::isfinite (ygrid))
         ygrid = (ylim[1] - ylim[0])/5.0;
     }
@@ -196,13 +205,14 @@ namespace Sixel {
     return *this;
   }
 
-  inline Plot& Plot::add_line (float x0, float y0, float x1, float y1, int colour_index, int stiple, float stiple_frac)
+  inline Plot& Plot::add_line (float x0, float y0, float x1, float y1,
+      int colour_index, int stiple, float stiple_frac)
   {
     if (!std::isfinite (xlim[0]) || !std::isfinite (xlim[1]))
-      set_xlim (std::min (x0, x1), std::max (x0, x1));
+      set_xlim (std::min (x0, x1), std::max (x0, x1), lim_expand_by_factor);
 
     if (!std::isfinite (ylim[0]) || !std::isfinite (ylim[1]))
-      set_ylim (std::min (y0, y1), std::max (y0, y1));
+      set_ylim (std::min (y0, y1), std::max (y0, y1), lim_expand_by_factor);
 
     x0 = mapx (x0);
     y0 = mapy (y0);
@@ -231,15 +241,16 @@ namespace Sixel {
 
 
   template <class VerticesType>
-    inline Plot& Plot::add_line (const VerticesType& y, int colour_index, int stiple, float stiple_frac)
+    inline Plot& Plot::add_line (const VerticesType& y,
+        int colour_index, int stiple, float stiple_frac)
     {
       if (!std::isfinite (xlim[0]) || !std::isfinite (xlim[1]))
-        set_xlim (0, y.size()-1);
+        set_xlim (0, y.size()-1, 0.0);
 
       if (!std::isfinite (ylim[0]) || !std::isfinite (ylim[1]))
-        set_ylim (*std::min_element (y.begin(), y.end()), *std::max_element (y.begin(), y.end()));
+        set_ylim (*std::min_element (y.begin(), y.end()), *std::max_element (y.begin(), y.end()), lim_expand_by_factor);
 
-      for (int n = 0; n < y.size()-1; ++n)
+      for (std::size_t n = 0; n < y.size()-1; ++n)
         add_line (n, y[n], n+1, y[n+1], colour_index, stiple, stiple_frac);
 
       return *this;
@@ -247,18 +258,19 @@ namespace Sixel {
 
 
   template <class VerticesType>
-    inline Plot& Plot::add_line (const VerticesType& x, const VerticesType& y, int colour_index, int stiple, float stiple_frac)
+    inline Plot& Plot::add_line (const VerticesType& x, const VerticesType& y,
+        int colour_index, int stiple, float stiple_frac)
     {
       if (x.size() != y.size())
         throw std::runtime_error ("number of x & y vertices do not match");
 
       if (!std::isfinite (xlim[0]) || !std::isfinite (xlim[1]))
-        set_xlim (*std::min_element (x.begin(), x.end()), *std::max_element (x.begin(), x.end()));
+        set_xlim (*std::min_element (x.begin(), x.end()), *std::max_element (x.begin(), x.end()), lim_expand_by_factor);
 
       if (!std::isfinite (ylim[0]) || !std::isfinite (ylim[1]))
-        set_ylim (*std::min_element (y.begin(), y.end()), *std::max_element (y.begin(), y.end()));
+        set_ylim (*std::min_element (y.begin(), y.end()), *std::max_element (y.begin(), y.end()), lim_expand_by_factor);
 
-      for (int n = 0; n < x.size()-1; ++n)
+      for (std::size_t n = 0; n < x.size()-1; ++n)
         add_line (x[n], y[n], x[n+1], y[n+1], colour_index, stiple, stiple_frac);
 
       return *this;
@@ -267,14 +279,16 @@ namespace Sixel {
 
 
 
-  Plot& Plot::add_text (const std::string& text, float x, float y, float anchor_x, float anchor_y, int colour_index)
+  Plot& Plot::add_text (const std::string& text, float x, float y,
+      float anchor_x, float anchor_y, int font_size, int colour_index)
   {
-    const int text_width = font.width() * text.size();
+    auto f = Font::get_font_for_size (font_size);
+    const int text_width = f.width() * text.size();
     int posx = std::round (margin_x + mapx (x) - anchor_x * text_width);
-    int posy = std::round (mapy (y) - (1.0-anchor_y) * font.height());
+    int posy = std::round (mapy (y) - (1.0-anchor_y) * f.height());
 
-    for (int n = 0; n < text.size(); ++n) {
-      font.render (canvas, text[n], posx+n*font.width(), posy, colour_index);
+    for (std::size_t n = 0; n < text.size(); ++n) {
+      f.render (canvas, text[n], posx+n*f.width(), posy, colour_index);
     }
 
     return *this;
--- plot_demo.cpp
+++ plot_demo.cpp
@@ -9,19 +9,17 @@ int main (int argc, char* argv[])
   std::vector<float> x (50);
   std::vector<float> y (50);
 
-  for (int n = 0; n < x.size();++n) {
+  for (std::size_t n = 0; n < x.size();++n) {
     y[n] = std::sin (0.2*n) + 0.3*std::cos (0.33*n);
     x[n] = 20.0+10.0*std::cos (0.41*n) + 5.0*std::sin(0.21*n);
   }
 
 
   std::cout << "Plotting arbitrary lines:" << std::endl;
-  Sixel::plot (768, 256, 6)
+  Sixel::plot (768, 256, 8)
     .add_line (y, 4, 10)
     .add_line (x, y, 3)
-    .add_text ("hello", 0, 0, 0, 1);
-  std::cout << "\n";
-  std::cout << "\n";
+    .add_text ("sinusoids", (x.size()-1)/2.0, 1.2, 0.5, 0.0, 12, 6);
 
 
   std::random_device rd;
@@ -36,7 +34,6 @@ int main (int argc, char* argv[])
   Sixel::plot (1024, 256)
     .set_grid (50, 2)
     .add_line (noise,2);
-  std::cout << "\n";
 
   return 0;
 }
