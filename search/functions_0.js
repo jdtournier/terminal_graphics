d4ef23ad5440b63b1dc95a6f4bf99aaa84eb2edd
J-Donald Tournier <jdtournier@gmail.com>
2024-09-12 15:32:32 +0100

    update doxygen docs

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['add_5fline_0',['add_line',['../classTG_1_1Plot.html#a3ab2fee9aa06e42899eaca6c1f917e20',1,'TG::Plot::add_line(float x0, float y0, float x1, float y1, int colour_index=2, int stiple=0, float stiple_frac=0.5)'],['../classTG_1_1Plot.html#a6de70e66e935ecd33b08382bce344737',1,'TG::Plot::add_line(const VerticesType &amp;y, int colour_index=2, int stiple=0, float stiple_frac=0.5)'],['../classTG_1_1Plot.html#a627d141317b5507380bf165129961531',1,'TG::Plot::add_line(const VerticesType &amp;x, const VerticesType &amp;y, int colour_index=2, int stiple=0, float stiple_frac=0.5)']]],
+  ['add_5fline_0',['add_line',['../classTG_1_1Plot.html#a3ab2fee9aa06e42899eaca6c1f917e20',1,'TG::Plot::add_line(float x0, float y0, float x1, float y1, int colour_index=2, int stiple=0, float stiple_frac=0.5)'],['../classTG_1_1Plot.html#a6de70e66e935ecd33b08382bce344737',1,'TG::Plot::add_line(const VerticesType &amp;y, int colour_index=2, int stiple=0, float stiple_frac=0.5)'],['../classTG_1_1Plot.html#a0f769f3fe4ff5772c8d47a9de687ae11',1,'TG::Plot::add_line(const VerticesTypeX &amp;x, const VerticesTypeY &amp;y, int colour_index=2, int stiple=0, float stiple_frac=0.5)']]],
   ['add_5ftext_1',['add_text',['../classTG_1_1Plot.html#a584ded57c796576ce233e75cdbd29e26',1,'TG::Plot']]]
 ];
